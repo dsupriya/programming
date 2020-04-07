4e15e10a3a88c7a91976d295857112157865bb40
dsupriya <supriyad570@gmail.com>
2020-04-07 22:06:42 +0530

    Create Remove-Duplicates-Recursively.java

---
+++ advanced-recursion/Remove-Duplicates-Recursively.java
@@ -0,0 +1,25 @@
+public class Solution {
+
+	public static String removeConsecutiveDuplicates(String s) {
+		// Write your code here
+        String temp;
+        
+        String ans;
+        if(s.length() ==1)
+        {
+            return s;
+        }
+        else
+        {
+            if(s.charAt(0)==s.charAt(1))
+                temp="";
+            else
+                temp=""+s.charAt(0);
+        }
+        ans= removeConsecutiveDuplicates(s.substring(1));
+        return (temp+ans);
+        
+
+	}
+
+}Remove Duplicates Recursively
