7048fec3e2344883308163d823d89a18ff62c978
dsupriya <supriyad570@gmail.com>
2022-04-27 22:17:12 +0530

    Update frog-jump.java

--- dynamic2/frog-jump.java
+++ dynamic2/frog-jump.java
@@ -1,8 +1,10 @@
+
 import java.lang.Math;
 public class Solution {
     public static int frogJump(int n, int heights[]) {
+	    
 
-        // Write your code here..
+        // Iterative solution
 		int [] dp = new int[n];
 		if (n==1)
 			return 0;
@@ -23,6 +25,32 @@ public class Solution {
 		return dp[n-1];
 		
 		
+    }
+
+}
+==========
+import java.lang.*;
+public class Solution {
+	
+    public static int frogJump(int n, int heights[]) {
+
+        // Write your code here..
+		// Brute Force
+		int i = n - 1;
+		if(i==0)
+		{
+			return 0;
+		}
+		if(i==1)
+		{
+			int diff = Math.abs(heights[i]-heights[i-1]);
+			return diff;
+		}
+		
+		int option1 = Math.abs(heights[i]-heights[i-1]) + frogJump(n-1,heights);
+		int option2 = Math.abs(heights[i]-heights[i-2]) + frogJump(n-2,heights);
+		
+		return Math.min(option1, option2);
     }
 
 }
