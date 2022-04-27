e6c1a468411bf85764f89acd7a4580a7c13dcaea
dsupriya <supriyad570@gmail.com>
2022-04-27 22:23:08 +0530

    Update frog-jump.java

--- dynamic2/frog-jump.java
+++ dynamic2/frog-jump.java
@@ -54,3 +54,47 @@ public class Solution {
     }
 
 }
+
+==========
+import java.lang.*;
+import java.util.*;
+public class Solution {
+	
+    public static int frogJump(int n, int heights[]) {
+
+        // Write your code here..
+	// recursive_dp
+		int [] dp = new int[n];
+		Arrays.fill(dp,-1);
+		frogJump_recursive_dp(n,heights,dp);
+		return dp[n-1];
+    }
+	public static int frogJump_recursive_dp(int n, int heights[], int dp[]) {
+
+        // Write your code here..
+		// Brute Force
+		int i = n - 1;
+		if(dp[i]!=-1)
+			return dp[i];
+		if(i==0)
+		{
+			dp[0] = 0;
+			return 0;
+		}
+		if(i==1)
+		{
+			
+			int diff = Math.abs(heights[i]-heights[i-1]);
+			dp[1] = diff;
+			return diff;
+		}
+		
+		int option1 = Math.abs(heights[i]-heights[i-1]) + frogJump_recursive_dp(n-1,heights,dp);
+		int option2 = Math.abs(heights[i]-heights[i-2]) + frogJump_recursive_dp(n-2,heights,dp);
+		int ans = Math.min(option1, option2);
+		dp[i] = ans;
+		return ans;
+    }
+
+
+}
